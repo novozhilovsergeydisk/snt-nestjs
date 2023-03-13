f22f6c44a1463fcbc30fc841552ff94f5145eb56
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2023-03-14 01:05:56 +0300

    Add files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,11 @@ import * as fs from "fs";
 // set_fs(fs);
 
 import * as xlsx from 'xlsx';
+import { forEachResolvedProjectReference } from 'ts-loader/dist/instances';
 
-console.log(xlsx.readFile);
+import * as crypto from 'crypto';
+
+// console.log(xlsx.readFile);
 
 // console.log({ set_fs });
 
@@ -34,15 +37,69 @@ export class AppController {
 
   @Get('admin/upload-xlsx')
   upload_xlsx() {
-    const wb = xlsx.readFile('./new-upload.xlsx');
+//  If you are using CommonJS, you can use the following "require"
+// const crypto = require('crypto');
+
+    const randomString1 = crypto.randomBytes(4).toString('hex');
+    console.log(randomString1);
+
+    const randomString2 = crypto.randomBytes(8).toString('hex');
+    console.log(randomString2);
+
+    const randomString3 = crypto.randomBytes(16).toString('hex');
+    console.log(randomString3);
+
+    function isString(val) {
+      return (typeof val === "string" || val instanceof String);
+    }
+
+    const parse = (filename) => {
+      const excelData = xlsx.readFile(filename);
+
+      return Object.keys(excelData.Sheets).map((name) => ({
+        name,
+        data: xlsx.utils.sheet_to_json(excelData.Sheets[name]),
+      }));
+    };
+    const res = parse('./upload-list.xlsx');
+
+    // console.log(res);
+
+    parse('./upload-list.xlsx').forEach(element => {
+      // console.log(element.data[0]);
+
+      // if (element.name = 'UploadList') {
+      //   const el = element.data[5];
+      //   console.log(el['__EMPTY']);
+      // }
+
+      // console.log(element.name);
+    });
+
+    const wb = xlsx.readFile('./clients.xlsx');
     const ws = wb.Sheets['TDSheet'];
-    const data = xlsx.utils.sheet_to_json(ws, { raw: false });
+    const data = xlsx.utils.sheet_to_json(ws);
+    // const cell = data[6]['ТСН СНТ "ЗАГОРЬЕ"'];
 
-  
+    // console.log(isString(cell))
+    //
+    // console.log(data[6]['ТСН СНТ "ЗАГОРЬЕ"']);
 
-    // console.log(ws);
+    // console.log(data);
+
+    // let result = {};
+
+    let clientData = {};
+    let __plot__ = '';
+    let __fio__ = '';
 
     for (let k in data) {
+
+      // const isNumber = Number.isInteger(Number(k));
+      //
+      // console.log({ isNumber })
+
+      if (Number(k) < 5) continue;
 
       // const data__ = data[k];
       //
@@ -51,7 +108,7 @@ export class AppController {
       //   console.log(key + ':' + k +': ' + value);
       // });
 
-      // console.log(data__)
+      // console.log(__fio__)
 
       // for (let k in data__) {
       //   console.log(k + ': ' + data__[k]);
@@ -59,8 +116,51 @@ export class AppController {
 
       // console.log(data[1070]);
 
-      // console.log(k + ': ' + data[k]);
+      const subject = data[k]['Subject'];
+      // const isNumber = Number.isInteger(cell);
+      const fio = data[k]['Name'];
+      const accrued = data[k]['Accrued'];
+      const paid = data[k]['Paid'];
+      const debt = data[k]['Debt'];
+      const overpayment = data[k]['Overpayment'];
+
+      if (fio !== undefined) {
+        __fio__ = fio;
+        __plot__ = subject;
+        // result[k] = { plot: subject, fio };
+        // result['fio'] = fio;
+        // console.log({ fio });
+      }
+
+      // console.log({ __fio__ })
+
+      if (subject !== 'Итого') {
+        let data = { id: k, __plot__, __fio__, subject, accrued, paid, debt, overpayment };
+        const uuid = crypto.randomBytes(16).toString('hex');
+
+        // console.log(typeof uuid);
+
+        clientData[uuid] = data;
+
+        // clientData[k] = { __fio__, subject, accrued, paid, debt, overpayment };
+
+        // console.log(k);
+        // console.log({ subject });
+        // console.log({ fio });
+        // console.log({ accrued });
+        // console.log({ paid });
+        // console.log({ debt });
+        // console.log({ overpayment });
+        // console.log({ isNumber });
+        // console.log('-------------------------------------');
+      } else {
+        console.log({ clientData });
+
+        console.log(subject + ' : ' + accrued + ' руб. начислено, ' + paid + ' руб. оплачено, ' + debt + ' руб. долг, ' + overpayment + ' руб. переплата.');
+      }
     }
+
+
 
     // console.log(ws);
 
