f4ef57d77b8c435b41902360e4e99c0266f74624
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2023-05-12 02:41:29 +0300

    Add files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
@@ -8,13 +8,18 @@ import { JwtModule } from '@nestjs/jwt';
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    UsersModule,
+    forwardRef(() => UsersModule),
     JwtModule.register({
       secret: process.env.PRIVATE_KEY || 'secret_key_jwt',
       signOptions: {
         expiresIn: '24h'
       }
     })
+  ],
+  /* Для возможности переиспользования */
+  exports: [
+    AuthService,
+    JwtModule
   ]
 })
 export class AuthModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from './user.model';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles-auth.decorator';
+import { RolesGuard } from '../auth/roles.guard';
 
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOperation({summary: 'Создание пользователя'})
+  @ApiResponse({status: 200, type: User})
   @Post()
   createUsers (@Body() userDto: CreateUserDto) {
     console.log({ userDto })
@@ -17,7 +24,12 @@ export class UsersController {
 
   }
 
+  @ApiOperation({summary: 'Получить всех пользователей'})
+  @ApiResponse({status: 200, type: [User]})
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
   @Get()
+  // @UseGuards(JwtAuthGuard)
   getAll() {
     // return {foo: 'bar'};
     return this.usersService.getAllUsers();
